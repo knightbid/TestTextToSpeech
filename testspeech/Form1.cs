d6f294f39b22d501fab0055c5db76ba155476767
IT: Leo.Luan <luan.leo@feavn.local>
2024-08-09 14:53:37 +0700

    Init Project

---
+++ testspeech/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace testspeech
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (InstalledVoice voice in synthesizer.GetInstalledVoices())
+            {
+                Console.WriteLine(voice.VoiceInfo.Name);
+            }
+            //synthesizer.SelectVoice();
+            synthesizer.Speak("1");
+        }
+    }
+}
